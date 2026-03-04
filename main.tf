fcff36bed2e392ae18a84e2bfce4375b8db22ab6
Juuso Ranne <juuso.ranne@protonmail.com>
2026-03-04 15:02:35 +0200

    added env functionality

--- main.tf
+++ main.tf
@@ -1,35 +1,49 @@
-# Create subscriptions for each solution
-resource "azurerm_subscription" "solutions" {
-  for_each = var.solution_name
+# Flatten solution-environment combinations
+locals {
+  solution_environments = merge([
+    for sol_key, sol in var.solution_name : {
+      for env in sol.environments :
+      "${sol_key}-${env}" => {
+        solution_key = sol_key
+        solution     = sol
+        environment  = env
+      }
+    }
+  ]...)
+}
 
-  subscription_name = each.value.name
-  alias             = each.value.name
+# Create subscriptions for each solution-environment combination
+resource "azurerm_subscription" "solutions" {
+  for_each = local.solution_environments
+
+  subscription_name = "${each.value.solution.name}-${each.value.environment}"
+  alias             = "${each.value.solution.name}-${each.value.environment}"
 }
 
 # Create resource groups in each subscription
 resource "azurerm_resource_group" "solutions" {
-  for_each = var.solution_name
+  for_each = local.solution_environments
 
-  name          = "rg-${each.value.name}"
-  location      = "West Europe"
+  name            = "rg-${each.value.solution.name}-${each.value.environment}"
+  location        = "West Europe"
   subscription_id = azurerm_subscription.solutions[each.key].id
 }
 
 # Conditionally create virtual networks when vnet = true
 resource "azurerm_virtual_network" "solutions" {
-  for_each = { for k, v in var.solution_name : k => v if v.vnet == true }
+  for_each = { for k, v in local.solution_environments : k => v if v.solution.vnet == true }
 
-  name                = "vnet-${each.value.name}"
+  name                = "vnet-${each.value.solution.name}-${each.value.environment}"
   resource_group_name = azurerm_resource_group.solutions[each.key].name
   location            = azurerm_resource_group.solutions[each.key].location
-  address_space       = ["10.0.0.0${each.value.cidr}"]
+  address_space       = ["10.0.0.0${each.value.solution.cidr}"]
 }
 
 # Conditionally create peering when peering = true and vnet = true
 resource "azurerm_virtual_network_peering" "solutions" {
-  for_each = { for k, v in var.solution_name : k => v if v.vnet == true && v.peering == true }
+  for_each = { for k, v in local.solution_environments : k => v if v.solution.vnet == true && v.solution.peering == true }
 
-  name                      = "peer-${each.value.name}"
+  name                      = "peer-${each.value.solution.name}-${each.value.environment}"
   resource_group_name       = azurerm_resource_group.solutions[each.key].name
   virtual_network_name      = azurerm_virtual_network.solutions[each.key].name
   remote_virtual_network_id = azurerm_virtual_network.solutions[each.key].id
@@ -37,9 +51,9 @@ resource "azurerm_virtual_network_peering" "solutions" {
 
 # Conditionally create route table when route = true and vnet = true
 resource "azurerm_route_table" "solutions" {
-  for_each = { for k, v in var.solution_name : k => v if v.vnet == true && v.route == true }
+  for_each = { for k, v in local.solution_environments : k => v if v.solution.vnet == true && v.solution.route == true }
 
-  name                = "rt-${each.value.name}"
+  name                = "rt-${each.value.solution.name}-${each.value.environment}"
   location            = azurerm_resource_group.solutions[each.key].location
   resource_group_name = azurerm_resource_group.solutions[each.key].name
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,32 @@
 variable "solution_name" {
   type = map(object({
-    name    = string
-    vnet    = bool
-    peering = bool
-    cidr    = string
-    route   = bool
-    githubOrg = string
+    name         = string
+    environments = list(string)
+    vnet         = bool
+    peering      = bool
+    cidr         = string
+    route        = bool
+    githubOrg    = string
   }))
 
   default = {
     solution1 = {
-      name    = "solution1"
-      vnet    = true
-      peering = true
-      cidr    = "/24"
-      route   = true
-      githubOrg = "defaultOrg"
+      name         = "solution1"
+      environments = ["dev", "test", "prod"]
+      vnet         = true
+      peering      = true
+      cidr         = "/24"
+      route        = true
+      githubOrg    = "defaultOrg"
     }
     solution2 = {
-      name    = "solution2"
-      vnet    = false
-      peering = false
-      cidr    = "/26"
-      route   = false
-      githubOrg = "defaultOrg"
+      name         = "solution2"
+      environments = ["dev", "test", "prod"]
+      vnet         = false
+      peering      = false
+      cidr         = "/26"
+      route        = false
+      githubOrg    = "defaultOrg"
     }
     }
   }
